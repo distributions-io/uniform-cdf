a098b6728be18d5e8734bf0e53a6ebfe919e3301
Planeshifter <pburckhardt@outlook.com>
2015-07-21 14:57:46 +0200

    [INIT]

---
+++ test/fixtures/test.array.R
@@ -0,0 +1,18 @@
+options( digits = 16 )
+library( jsonlite )
+
+a = 0
+b = 1
+x = seq( -1000, 1000, 0.5 )
+y = punif( x, a,b )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	a = a,
+	b = b,
+	data = x,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/array.json" )
---
+++ test/fixtures/test.matrix.R
@@ -0,0 +1,19 @@
+options( digits = 16 )
+library( jsonlite )
+
+a = 0
+b = 1
+x = 0:24
+y = punif( x, a,b )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	a = a,
+	b = b,
+	data = x,
+	expected = y
+)
+
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/matrix.json" )
